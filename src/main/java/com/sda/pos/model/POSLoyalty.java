0668ce19fcd75857b9dc95e96ee54833fd0dba3c
Juste <justebarto@gmail.com>
2022-12-27 15:39:58 +0200

    add posloyalty

--- src/main/java/com/sda/pos/model/POSLoyalty.java
+++ src/main/java/com/sda/pos/model/POSLoyalty.java
@@ -13,8 +13,63 @@ import java.util.Objects;
 @Entity
 public class POSLoyalty {
     @Id
-    private long userId;
+    private Long userId;
     private String number;
     private Timestamp startDate;
     private Timestamp expirationDate;
+
+    public POSLoyalty() {
+    }
+
+    public POSLoyalty(Long userId, String number, Timestamp startDate, Timestamp expirationDate){
+        this.userId = userId;
+        this.number = number;
+        this.startDate = startDate;
+        this.expirationDate = expirationDate;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public Timestamp getStartDate() {
+        return startDate;
+    }
+
+    public Timestamp getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public void setStartDate(Timestamp startDate) {
+        this.startDate = startDate;
+    }
+
+    public void setExpirationDate(Timestamp expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        POSLoyalty that = (POSLoyalty) o;
+        return Objects.equals(userId, that.userId) && Objects.equals(number, that.number) && Objects.equals(startDate, that.startDate) && Objects.equals(expirationDate, that.expirationDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, number, startDate, expirationDate);
+    }
 }
