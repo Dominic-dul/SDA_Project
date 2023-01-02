bf550cfc7cff9a43c7f72501ea0081194b87ba09
Donatas Pučinskas <pucinskas.donatas@gmail.com>
2023-01-02 11:35:39 +0200

    Implement employee service

--- src/main/java/com/sda/pos/service/EmployeeServiceImpl.java
+++ src/main/java/com/sda/pos/service/EmployeeServiceImpl.java
@@ -5,6 +5,8 @@ import com.sda.pos.model.POSEmployee;
 import com.sda.pos.repository.EmployeeRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.NoSuchElementException;
+
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
@@ -16,16 +18,22 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public POSEmployee getEmployeeByEmployeeId(Long employeeId) {
-        return null;
+        return employeeRepository.findById(employeeId).orElseThrow();
     }
 
     @Override
     public void saveEmployee(Long employeeId, EmployeeRole employeeRole, Long storeId, String fullName, String address) {
-
+        POSEmployee newEmployee = new POSEmployee(employeeId, employeeRole, storeId, fullName, address);
+        employeeRepository.save(newEmployee);
     }
 
     @Override
     public void deleteEmployeeByEmployeeId(Long employeeId) {
-
+        if(employeeRepository.existsById(employeeId)){
+            employeeRepository.deleteById(employeeId);
+        }
+        else {
+            throw new NoSuchElementException("Employee with this id does not exist");
+        }
     }
 }
